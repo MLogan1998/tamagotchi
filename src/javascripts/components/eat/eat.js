f0caa2201a123dc56d76868a289d23881ac2553d
Matt Logan <mlogan5212@gmail.com>
2020-06-03 21:26:13 -0500

    completed eat component

--- src/javascripts/components/eat/eat.js
+++ src/javascripts/components/eat/eat.js
@@ -1,9 +1,31 @@
 import './eat.scss';
 import utils from '../../utils/utils';
 
+let full = 100;
+
 const testEat = () => {
-  const domstring = 'eeeeeat!';
+  const domstring = `
+    <h1>Eat!</h1>
+    <h2>Fullness Score: ${full}</h2>
+    <button class="healthy">Healthy Food</button><button class="junk">Junk Food</button>`;
   utils.printToDom('#eat', domstring);
 };
+
+const eatHealthy = () => {
+  if (full === 100) return;
+  full += 10;
+  if (full > 100) full = 100;
+  testEat();
+};
+
+const eatJunk = () => {
+  if (full === 0) return;
+  full -= 3;
+  if (full < 0) full = 0;
+  testEat();
+};
+
+$('body').on('click', '.healthy', eatHealthy);
+$('body').on('click', '.junk', eatJunk);
 
 export default { testEat };
